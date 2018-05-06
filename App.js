36495075868cd4dc11973f6186859443ffa9877a
Marvin Dawson <marvintdawson@gmail.com>
2018-05-06 10:27:58 -0500

    awesome places input and layout updated

--- App.js
+++ App.js
@@ -1,23 +1,79 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends React.Component {
+
+    state = {
+            placeName: "",
+            places: []
+        };
+
+    placeNameChangedHandler = val => {
+
+        this.setState({
+             placeName: val
+         });
+    };
+
+    placeSubmitHandler =  () => {
+        if (this.state.placeName.trim() === ""){
+            return;
+        }
+
+        this.setState(prevState => {
+            return {
+                places: prevState.places.concat(prevState.placeName)
+            };
+        });
+    };
+
   render() {
+      const placesOutput = this.state.places.map((place, i) =>
+          <Text key={i}>{place}</Text>
+      );
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+          <View style={styles.inputViewContainer}>
+              <TextInput style={styles.addPlaceInput}
+                         placeholder='Add A Awesome Place Buddy'
+                         value={this.state.placeName}
+                         onChangeText={this.placeNameChangedHandler} />
+              <Button
+                  onPress={this.placeSubmitHandler}
+                  style={styles.button}
+                  title="Add"  />
+          </View>
+          <View>
+              {placesOutput}
+          </View>
       </View>
     );
   }
-}
+};
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+      padding: 26,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+    inputViewContainer: {
+     //  flex: 1,
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    addPlaceInput: {
+        width: "80%"
+    },
+    button:{
+        width: "20%"
+    }
 });
